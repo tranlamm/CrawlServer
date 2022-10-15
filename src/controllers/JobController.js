3dbc9fda02c84251a4cc7ddf55bdb47a05036521
tranlamm <tranlam021102eee@gmail.com>
2022-10-15 12:45:46 +0700

    update

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -118,7 +118,7 @@ const jobController = {
         const skills = [];
         const { urls, names } = await jobController.getAllSkillName();
         try {
-            for (let i = 0; i < 77; i++) {
+            for (let i = 0; i < 30; i++) {
                 const { data } = await axios.get(urls[i]);
                 const $ = cheerio.load(data);
                 const amount = $('.search-page__jobs .search-page__jobs-inner #jobs > h1').text().trim().split(' ')[0];
